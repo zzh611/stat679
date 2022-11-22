10ead2bb581cf74f53f8d2513c99c574bc6a8dc8
Kelly Zihan Zhao <48823712+zzh611@users.noreply.github.com>
2022-11-21 18:11:13 -0600

    update  milestone 2 code

--- Milestone2Code.R
+++ Milestone2Code.R
@@ -8,7 +8,11 @@ library(ggplot2)
 library(reshape2)
 library(scales)
 
-## Crime map
+## A.Crime map
+### Shiny app, you can find the visualization here
+### https://zhaozihan.shinyapps.io/Crimemap/
+
+### data processing
 crimes <- read.csv("Crimes_With_Dates_Cleaned.csv")
 crime.lite <- crimes %>% 
   select(Longitude, Latitude, Crime.Name3) %>% 
@@ -16,6 +20,7 @@ crime.lite <- crimes %>%
   mutate(Type = Crime.Name3) %>% 
   select(-Crime.Name3)
 
+### visual function
 viz_map <- function(df) {
   df %>%
     leaflet() %>% 
@@ -48,20 +53,24 @@ server <- function(input, output, session) {
 shinyApp(ui, server)
 
 
-## Yearly change
+## B.Yearly change
+### Data processing
 data1 <- data.frame(crimes[,32],crimes[,8:10])
 
 data2 <- data1 %>%
   group_by(Crime.Name1) %>%
   count(Year)
 
+### Time series plot
 ggplot(data2) +
   geom_line(aes(Year, n, col = Crime.Name1))
 
+### interactive plot with plotly
 fig <- plot_ly(data2, x = ~Year, y = ~n, type = 'scatter', mode = 'lines', color = ~Crime.Name1)
 fig
 
-## Crime type analysis
+## C.Crime type analysis
+### data processing
 crime_1 <- crimes$`Crime Name1` 
 crime_2 <- crimes$`Crime Name2` 
 crime_3 <- crimes$`Crime Name3` 
@@ -70,11 +79,25 @@ Place <- crimes$Place
 City <- crimes$City 
 State <- crimes$State 
 Agency <- crimes$Agency
-crime_type = data.frame(crime_1, crime_2, crime_3, Year, Place, City, State, Agency)
+crime_type = data.frame(crime_1, crime_2, crime_3, 
+                        Year, Place, City, State, Agency)
 
-## Crime time analysis
+### bar plot
+crime_type %>%
+  ggplot() +
+  geom_bar(aes(crime_1, fill=crime_1)) +
+  facet_wrap(~ Year) +
+  scale_color_discrete(labels = label_wrap(15)) +
+  xlab("Crime Type") + 
+  ylab("Counts") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+        legend.text=element_text(size=8))
+
+## E.Crime time analysis
+### data processing
 df <- melt(crimes ,  id.vars = 'hours', variable.name = 'counts')
 
+### area plot
 ggplot(data = df,aes(x=hours)) +
   geom_area(aes(y = value, fill=counts),  alpha=0.6) +
   ggtitle("Total Crime and Victims counts by Hour") +
